013702e8af1207b1737873f5178fef8c39781128
Cristian Paul <apostolmorondoz12@gmail.com>
2025-03-20 20:56:04 +0800

    Updated add_task.php and added new files

--- add_task.php
+++ add_task.php
@@ -4,7 +4,7 @@ include "db.php";
 
 if (isset($_POST['task_name']) && !empty($_POST['task_name'])) {
     $task_name = $_POST['task_name'];
-    $user_id = $_SESSION['user_id']; // Get the user_id from the session
+    $user_id = $_SESSION['user_id']; 
 
     $query = $conn->prepare("INSERT INTO tasks (task_name, user_id) VALUES (:tname, :uid)");
     $query->bindParam(":tname", $task_name);
